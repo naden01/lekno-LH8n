1a2aac4fb126b57d79d55227aca10a022fe36d81
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-09-09 07:19:32 -0700

    Update

--- twrp_LH8n.mk
+++ twrp_LH8n.mk
@@ -9,20 +9,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+#Inherit emulated_storage properties
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TECNO-LH8n device
-$(call inherit-product, device/tecno/TECNO-LH8n/device.mk)
+$(call inherit-product, device/tecno/LH8n/device.mk)
 
-PRODUCT_DEVICE := TECNO-LH8n
-PRODUCT_NAME := omni_TECNO-LH8n
+PRODUCT_DEVICE := LH8n
+PRODUCT_NAME := twrp_LH8n
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO LH8n
 PRODUCT_MANUFACTURER := tecno
 
 PRODUCT_GMS_CLIENTID_BASE := android-tecno
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vnd_lh8nu_h333-user 12 SP1A.210812.016 677798 release-keys"
 
