02513f972421e60a423e89fcf7ac62bb2f4c6cdb
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-09-10 02:27:13 -0700

    Update

--- twrp_LH8n.mk
+++ twrp_LH8n.mk
@@ -7,7 +7,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 #Inherit emulated_storage properties
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
