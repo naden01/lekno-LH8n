f2927abe4e1605d7c6a41bc6e59f9ed2dacea301
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-09-16 07:15:20 -0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,9 +4,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_LH8n.mk
 
 COMMON_LUNCH_CHOICES := \
+    twrp_LH8n-user \
+    twrp_LH8n-userdebug \
     twrp_LH8n-eng
