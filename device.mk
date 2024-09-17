3abf5152eab213fbecc49d40b62036d188d1ed8c
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-09-17 07:08:51 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/infinix/X6711
+LOCAL_PATH := device/tecno/LH8n
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
